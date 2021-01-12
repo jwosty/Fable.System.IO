f2d6619642cc5aae098796bf764c4f67d4978d61
John Wostenberg <jwosty@users.noreply.github.com>
2021-01-12 13:53:17 -0600

    Add Path.IsPathRooted

--- src/Fable.System.IO.fs
+++ src/Fable.System.IO.fs
@@ -4,6 +4,14 @@ open System
 
 module IO =
     type path(separator: string) =
+        member this.IsPathRooted (path: string) =
+            if path.Length = 0 then false
+            else if path.[0] = '/' || path.[0] = '\\' then true
+            else if path.Length = 1 then false
+            else
+                let driveLetter = path.[0]
+                (path.[1] = ':') && (int driveLetter) >= (int 'A') && (int driveLetter) <= (int 'z')
+        
         member this.Combine ([<ParamArray>] paths: string[]) =
             String.Join (separator, paths)
 
--- tests/Fable.System.IO.Tests.fs
+++ tests/Fable.System.IO.Tests.fs
@@ -11,6 +11,73 @@ let arrayToStr (array: 'a[]) =
     "[|" + System.String.Join (";", array) + "|]"
 
 let ``Fable.System.IO.Path.Tests`` =
+    let isPathRootedTests =
+        let testCases = [
+            "empty path", "", false
+            "simple unix-style relative path", "foo/bar", false
+            "simple windows-style relative path", "foo\\bar", false
+            "simple mixed-style relative path", "foo/bar\\baz", false
+
+            "explicit current dir", ".", false
+            "explicit parent dir", "..", false
+            "explicit current dir with trailing unix path separator", "./", false
+            "explicit current dir with trailing windows path separator", ".\\", false
+            "explicit parent dir with trailing unix path separator", "../", false
+            "explicit parent dir with trailing windows path separator", "..\\", false
+
+            "unix root", "/", true
+            "simple unix absolute path", "/foo", true
+            "simple unix absolute path with trailing separators", "/foo/", true
+            "simple unix absolute path with extra beginning separators", "//foo", true
+            
+            "windows C drive 1 no slash", "C:", true
+            "windows C drive 2 slash", "C:\\", true
+            "windows C drive subfolder", "C:\\foo", true
+            "windows C drive forward slash", "C:/", true
+            "windows C drive subfolder no slash", "C:foo", true
+            "windows style root no drive letter", "\\", true
+            "windows style root subfolder no drive letter", "\\foo", true
+            "windows D drive forward slash", "D://", true
+            "windows z drive slash", "z:\\", true
+            "windows Z drive slash", "Z:\\", true
+            "windows a drive slash", "a:\\", true
+            "windows A drive slash", "A:\\", true
+
+            "UNC named pipe", "\\.\\pipe\\MyPipe", true
+            
+            "strange path", "::", false
+        ]
+        testList "IsPathRooted" [
+            testList "IndependentTests" [
+                for (caseName, input, expected) in testCases ->
+                    testList caseName [
+                        testCase "Windows" (fun () ->
+                            let actual = Fable.Windows.System.IO.Path.IsPathRooted input
+                            Expect.equal actual expected "Path.IsPathRooted Windows"
+                        )
+                        testCase "Unix" (fun () ->
+                            let actual = Fable.Unix.System.IO.Path.IsPathRooted input
+                            Expect.equal actual expected "Path.IsPathRooted Unix"
+                        )
+                    ]
+            ]
+            testList "OracleTests" [
+                for (caseName, input, expected) in testCases ->
+                    testList caseName [
+                        if RuntimeInformation.IsOSPlatform OSPlatform.Windows then
+                            testCase "Windows" (fun () ->
+                                let oracle = global.System.IO.Path.IsPathRooted input
+                                Expect.equal expected oracle "Path.Combine Windows (Oracle)"
+                            )
+                        else
+                            testCase "Unix" (fun () ->
+                                let oracle = global.System.IO.Path.IsPathRooted input
+                                Expect.equal expected oracle "Path.Combine Unix (Oracle)"
+                            )
+                    ]
+            ]
+        ]
+
     let combineTests =
         let testCases = [
             [|"foo"|], "foo", "foo"
@@ -22,11 +89,11 @@ let ``Fable.System.IO.Path.Tests`` =
                     testList (arrayToStr input) [
                         testCase "Windows" (fun () ->
                             let actual = Fable.Windows.System.IO.Path.Combine input
-                            Expect.equal actual windowsExpected "Path.Combine windows"
+                            Expect.equal actual windowsExpected "Path.Combine Windows"
                         )
                         testCase "Unix" (fun () ->
                             let actual = Fable.Unix.System.IO.Path.Combine input
-                            Expect.equal actual unixExpected "Path.Combine unix"
+                            Expect.equal actual unixExpected "Path.Combine Unix"
                         )
                     ]
             ]
@@ -52,6 +119,7 @@ let ``Fable.System.IO.Path.Tests`` =
         ]
 
     testList "Path" [
+        isPathRootedTests
         combineTests
     ]
 
