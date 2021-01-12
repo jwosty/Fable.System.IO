f7d7d2d7bac8ff81f825fe0c8fe16fb2a2c8554b
John Wostenberg <jwosty@users.noreply.github.com>
2021-01-12 12:06:34 -0600

    Add oracle tests

--- tests/Fable.System.IO.Tests.fs
+++ tests/Fable.System.IO.Tests.fs
@@ -1,5 +1,6 @@
 module Fable.System.IO.Tests
 open System
+open System.Runtime.InteropServices
 #if FABLE_COMPILER
 open Fable.Mocha
 #else
@@ -10,25 +11,48 @@ let arrayToStr (array: 'a[]) =
     "[|" + System.String.Join (";", array) + "|]"
 
 let ``Fable.System.IO.Path.Tests`` =
-    testList "Path" [
-        testList "Combine" [
-            let testCases = [
-                [|"foo"|], "foo", "foo"
-                [|"foo"; "bar"|], "foo/bar", "foo\\bar"
-            ]
-
-            for (input, unixExpected, windowsExpected) in testCases ->
-                testList (arrayToStr input) [
-                    testCase "Windows" (fun () ->
-                        let actual = Fable.Unix.System.IO.Path.Combine input
-                        Expect.equal actual unixExpected "Path.Combine unix"
-                    )
-                    testCase "Unix" (fun () ->
-                        let actual = Fable.Windows.System.IO.Path.Combine input
-                        Expect.equal actual windowsExpected "Path.Combine windows"
-                    )
-                ]
+    let combineTests =
+        let testCases = [
+            [|"foo"|], "foo", "foo"
+            [|"foo"; "bar"|], "foo/bar", "foo\\bar"
         ]
+        testList "Combine" [
+            testList "IndependentTests" [
+                for (input, unixExpected, windowsExpected) in testCases ->
+                    testList (arrayToStr input) [
+                        testCase "Windows" (fun () ->
+                            let actual = Fable.Windows.System.IO.Path.Combine input
+                            Expect.equal actual windowsExpected "Path.Combine windows"
+                        )
+                        testCase "Unix" (fun () ->
+                            let actual = Fable.Unix.System.IO.Path.Combine input
+                            Expect.equal actual unixExpected "Path.Combine unix"
+                        )
+                    ]
+            ]
+            testList "OracleTests" [
+                // these tests compare the output to the BCL implementation to verify that they match for a particular
+                // platform
+                for (input, _, _) in testCases ->
+                    testList (arrayToStr input) [
+                        if RuntimeInformation.IsOSPlatform OSPlatform.Windows then
+                            testCase "Windows" (fun () ->
+                                let actual = Fable.Windows.System.IO.Path.Combine input
+                                let expected = global.System.IO.Path.Combine input
+                                Expect.equal actual expected "Path.Combine Windows (Oracle)"
+                            )
+                        else
+                            testCase "Unix" (fun () ->
+                                let actual = Fable.Unix.System.IO.Path.Combine input
+                                let expected = global.System.IO.Path.Combine input
+                                Expect.equal actual expected "Path.Combine Unix (Oracle)"
+                            )
+                    ]
+            ]
+        ]
+
+    testList "Path" [
+        combineTests
     ]
 
 [<Tests>]
