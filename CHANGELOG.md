1c4e02663f0f28c51308f2111be6eaa5ecaae860
John Wostenberg <jwosty@users.noreply.github.com>
2021-01-20 23:02:26 -0600

    Create nuget package

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.1.0
+
+* Initial version - implements most of System.IO.Path
+
+##
--- build.fsx
+++ build.fsx
@@ -27,26 +27,97 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Tools
 
+Trace.log "line 30!"
+
 let yarnCmd =
     lazy (
-        match ProcessUtils.tryFindFileOnPath "yarn" with
-        | Some yarn -> yarn
-        | None -> failwith "cmd not found: yarn")
+        Trace.log "GOT HERE yarnCmd"
+        let result =
+            match ProcessUtils.tryFindFileOnPath "yarn" with
+            | Some yarn -> yarn
+            | None -> failwith "cmd not found: yarn"
+        Trace.logfn "DONE yarnCmd: %s" result
+        result
+        )
 
 let project = Path.Combine ("src", "Fable.System.IO.fsproj")
 let testProject = Path.Combine ("tests", "Fable.System.IO.Tests.fsproj")
 let solution = Path.Combine (".", "Fable.System.IO.sln")
+
+let artifactsDir = Path.Combine (".", "artifacts")
 
 let runDotNet cmd workingDir =
     let result =
         DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
+type PackageVersionInfo = { versionName: string; versionChanges: string }
+
+let scrapeChangelog () =
+    let changelog = System.IO.File.ReadAllText "CHANGELOG.md"
+    Trace.logfn "changelog text: %O" changelog
+    let regex = Regex("""## (?<Version>.*)\n+(?<Changes>(.|\n)*?)##""")
+    let result = seq {
+        for m in regex.Matches changelog ->
+            {   versionName = m.Groups.["Version"].Value.Trim()
+                versionChanges =
+                    m.Groups.["Changes"].Value.Trim()
+                        .Replace("    *", "    ◦")
+                        .Replace("*", "•")
+                        .Replace("    ", "\u00A0\u00A0\u00A0\u00A0") }
+    }
+    result
+
+let changelog = scrapeChangelog () |> Seq.toList
+Trace.logfn "changelog: %O"
+let currentVersionInfo =
+    List.tryHead changelog
+    |> Option.defaultWith (fun () -> failwithf "Version info not found!")
+Trace.logfn "currentVersionInfo: %O" currentVersionInfo
+
+let addProperties props (defaults) =
+    { defaults with MSBuild.CliArguments.Properties = [yield! defaults.Properties; yield! props]}
+
+let addVersionInfo (versionInfo: PackageVersionInfo) options =
+    let versionPrefix, versionSuffix =
+        match String.splitStr "-" versionInfo.versionName with
+        | [hd] -> hd, None
+        | hd::tl -> hd, Some (String.Join ("-", tl))
+        | [] -> failwith "Version name is missing"
+    addProperties [
+        "VersionPrefix", versionPrefix
+        match versionSuffix with Some versionSuffix -> "VersionSuffix", versionSuffix | _ -> ()
+        "PackageReleaseNotes", versionInfo.versionChanges
+    ] options
+
+let buildCfg = DotNet.BuildConfiguration.fromEnvironVarOrDefault "configuration" DotNet.Release
+let defaultMsbuildParams msbuildParams = msbuildParams |> addVersionInfo currentVersionInfo
+
+let mkDefaultBuildOptions (options: DotNet.BuildOptions) =
+    { options with
+        Configuration = buildCfg
+        MSBuildParams = defaultMsbuildParams options.MSBuildParams
+    }
+
+let mkDefaultPackOptions (options: DotNet.PackOptions) =
+    { options with
+        Configuration = buildCfg
+        MSBuildParams = defaultMsbuildParams options.MSBuildParams
+    }
+
+let mkDefaultTestOptions (options: DotNet.TestOptions) =
+    { options with
+        Configuration = buildCfg
+        MSBuildParams = defaultMsbuildParams options.MSBuildParams
+    }
+
 Target.create "Clean" (fun _ ->
     Trace.log " --- Cleaning --- "
     
     DotNet.exec id "clean" "" |> ignore
-    File.deleteAll !!"**/*.fs.js"
+    File.deleteAll !!(Path.Combine("**","*.fs.js"))
+    
+    Shell.cleanDir artifactsDir
 )
 
 Target.create "Restore" (fun _ ->
@@ -54,23 +125,26 @@ Target.create "Restore" (fun _ ->
     DotNet.exec id "tool" "restore" |> Trace.logfn "%O"
     DotNet.exec id "paket" "restore" |> Trace.logfn "%O"
     DotNet.restore id |> Trace.logfn "%O"
-    Shell.Exec (yarnCmd.Force(), "install") |> ignore
+    Shell.Exec (yarnCmd.Value, "install") |> ignore
 )
 
 Target.create "Build" (fun _ ->
     Trace.log " --- Building --- "
-    DotNet.build id solution
+    project |> DotNet.build mkDefaultBuildOptions
 )
 
 Target.create "Test" (fun _ ->
     Trace.log " --- Running tests --- "
-    DotNet.test id solution
-    Shell.Exec (yarnCmd.Force(), "test") |> ignore
+    DotNet.test mkDefaultTestOptions solution
+    Shell.Exec (yarnCmd.Value, "test") |> ignore
 )
 
 Target.create "Pack" (fun _ ->
     Trace.log " --- Packing NuGet packages --- "
-    raise (NotImplementedException())
+    project
+    |> DotNet.pack (
+        mkDefaultPackOptions
+        >> (fun options -> { options with OutputPath = Some artifactsDir }))
 )
 
 Target.create "TestAll" ignore
