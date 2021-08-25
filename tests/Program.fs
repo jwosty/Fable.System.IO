549f39a8590112b3ef79c447a689f297b49f462f
John Wostenberg <jwosty@users.noreply.github.com>
2021-08-25 07:56:44 -0500

    Fix .NET tests

--- tests/Program.fs
+++ tests/Program.fs
@@ -17,16 +17,20 @@ module EntryPoint =
             Fable.System.IO.File.tests
         ]
 
+#if FABLE_COMPILER
     [<Import("XMLHttpRequest", "xmlhttprequest")>]
     let XMLHttpRequest: obj = jsNative
 
     [<Emit("global.XMLHttpRequest = $0")>]
     let setGlobalXMLHttpRequest (xmlHttpRequest: obj) = jsNative
+#endif
 
     [<EntryPoint>]
     let main args =
+#if FABLE_COMPILER
         // Now, anything that uses XMLHttpRequest will work!
         setGlobalXMLHttpRequest XMLHttpRequest
+#endif
         
         // Notice how we never actually reference Fable.System.IO.Path from the test suite, and are thus able to avoid
         // issue #5 (https://github.com/jwosty/Fable.System.IO/pull/7), as we don't need to test platform detection,
